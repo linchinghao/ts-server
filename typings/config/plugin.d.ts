b2da5544f13284b637f2bb46603a53981fb78d5c
Allin <linchinghao922@gmail.com>
2018-05-07 23:57:58 +0800

    refactor: delete

--- typings/config/plugin.d.ts
+++
@@ -1,9 +0,0 @@
-// This file was auto created by egg-ts-helper
-// Do not modify this file!!!!!!!!!
-
-import 'egg-passport';
-import 'egg-view-nunjucks';
-import 'egg-mongoose';
-import 'egg-socket.io';
-import 'egg-redis';
-import 'egg-view-assets';
