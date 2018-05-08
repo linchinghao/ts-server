6901f7462d8b579e4b217918ccd834685162ae41
Allin <linqhc@yonyou.com>
2018-05-08 19:59:07 +0800

    refactor: update jwt

--- app.ts
+++ app.ts
@@ -1,49 +1,70 @@
 import { Application, Context } from 'egg';
-import { Strategy as LocalStrategy } from 'passport-local';
+import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt';
+// import { Strategy as LocalStrategy } from 'passport-local';
 
 export default function(app: Application) {
+
   app.beforeStart(async () => {
     // 启动自定义
-
-    // 使用passport-local策略
-    app.passport.use(new LocalStrategy({
-      passReqToCallback: true,
-    }, (req, username, password, done) => {
-
-      const user = {
-        provider: 'local',
-        username,
-        password,
-      };
-
-      app.passport.doVerify(req, user, done);
-    }));
-
-    // 处理用户信息
-    app.passport.verify(async (ctx: Context, user: any) => {
-      // 验证
-      if (user.username === 'allin') {
-        return user;
-      } else {
-        this.ctx.throw(401, '授权失败!');
-      }
-    });
-
-    // 将用户信息序列化后存进 session 里面，一般需要精简，只保存个别字段
-    app.passport.serializeUser(async (ctx: Context, user: any) => {
-      return user;
-    });
-
-    // 反序列化后把用户信息从 session 中取出来，反查数据库拿到完整信息
-    app.passport.deserializeUser(async (ctx: Context, user: any) => {
-      return user;
-    });
-
+    console.log('#app before start');
   });
-};
 
-declare module 'egg' {
-  interface Application {
-    customerName: string;
-  }
-}
+  /* passport-jwt start */
+  const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: app.config.keys,
+  };
+  app.passport.use(new JwtStrategy(opts, (req: any, payload: any, done: any) => {
+    const user = {
+      provider: 'jwt',
+      payload,
+    };
+    app.passport.doVerify(req, user, done);
+  }));
+
+  app.passport.verify(async (ctx: Context, user: any) => {
+    // 验证
+    console.log('#auth', user);
+    if (user.username === 'allin') {
+      return user;
+    } else {
+      this.ctx.throw(401, '授权失败!');
+    }
+  });
+
+  /* passport-local start */
+  // 使用passport-local策略
+  // app.passport.use(new LocalStrategy({
+  //   passReqToCallback: true,
+  // }, (req, username, password, done) => {
+
+  //   const user = {
+  //     provider: 'local',
+  //     username,
+  //     password,
+  //   };
+
+  //   app.passport.doVerify(req, user, done);
+  // }));
+
+  // // 处理用户信息
+  // app.passport.verify(async (ctx: Context, user: any) => {
+  //   // 验证
+  //   if (user.username === 'allin') {
+  //     return user;
+  //   } else {
+  //     this.ctx.throw(401, '授权失败!');
+  //   }
+  // });
+
+  // // 将用户信息序列化后存进 session 里面，一般需要精简，只保存个别字段
+  // app.passport.serializeUser(async (ctx: Context, user: any) => {
+  //   return user;
+  // });
+
+  // // 反序列化后把用户信息从 session 中取出来，反查数据库拿到完整信息
+  // app.passport.deserializeUser(async (ctx: Context, user: any) => {
+  //   return user;
+  // });
+  /* passport-local end */
+};
--- app/router.ts
+++ app/router.ts
@@ -17,13 +17,13 @@ export default (app: Application) => {
   router.get('/user', auth, user.index);
 
   // 鉴权成功后的回调页面
-  router.get('/authCallback', home.authCallback);
+  // router.get('/authCallback', home.authCallback);
 
   // 渲染登录页面，用户输入账号密码
   router.get('/login', home.login);
 
   // 登录校验
-  router.post('/login', app.passport.authenticate('local', { successRedirect: '/authCallback' }));
+  // router.post('/login', app.passport.authenticate('local', { successRedirect: '/authCallback' }));
 
   router.get('/logout', home.logout);
 
